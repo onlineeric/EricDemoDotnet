c493156b158a7ea623cbf1c54c15203445da23b3
Eric Cheng <online.eric@gmail.com>
2024-01-12 21:21:20 +1300

    minor updates

--- src/EricMinimalApi/Program.cs
+++ src/EricMinimalApi/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/status", () => "V1 is running");
+app.MapGet("/", () => "Welcome to Eric's Minimal API");
 
 app.Run();
