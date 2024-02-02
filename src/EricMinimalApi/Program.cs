2241c11b33e664aedd5fd05fc242734707cf4209
Eric Cheng <online.eric@gmail.com>
2024-02-02 23:18:25 +1300

    minor update

--- src/EricMinimalApi/Program.cs
+++ src/EricMinimalApi/Program.cs
@@ -6,7 +6,7 @@ WebApplication app = builder.Build();
 string appVersion = builder.Configuration["AppVersion"];
 
 app.MapGet("/status", () => $"Version {appVersion} is running");
-app.MapGet("/", () => "Welcome to Eric's Minimal API");
+app.MapGet("/", () => "Welcome to Eric's Minimal API Demo");
 
 SimpleEndpoints.Map(app);
 BenchmarkEndpoints.Map(app);
